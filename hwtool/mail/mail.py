949d3206ed9638e7de69d4c9f1339bef015e974f
QuadnucYard <2380433991@qq.com>
2023-11-08 22:55:10 +0800

    Minor fixes

--- hwtool/mail/mail.py
+++ hwtool/mail/mail.py
@@ -20,7 +20,7 @@ def get_different_files(source: Path, base: Path):
             yield f
 
 
-email_template = Template(Path("email_template.jinja").read_text(encoding="utf-8"))
+email_template = Template(Path("mail/email_template.jinja").read_text(encoding="utf-8"))
 
 
 @app.command()
@@ -36,19 +36,21 @@ def main(result_path: str, subject: str, *, collect_path: str):
         for idx, row in df.iterrows():
             folder_name = f"{row['学号']}{row['姓名']}"
             receiver = f"{row['一卡通号']}@seu.edu.cn"
-            main_score = f"评分：{row['分数']} = {row['基础分']} + {row['附加分']}"
+            main_score = f"评分：{row['分数']} = {row['基础分']} + {row['附加分']}" if "附加分" in row else f"评分：{row['基础分']}"
+            # 附件
+            att = list(get_different_files(folder / folder_name, collect_folder / folder_name))
+            # 设置内容
             content = email_template.render(
                 main_score=main_score,
                 scores=row[i1:i2].to_dict(),
                 remarks=markdown(str(row["评语"]).replace("\n", "  \n")),
+                attach=len(att) > 0,
             )
-            # 附件
-            att = list(get_different_files(folder / folder_name, collect_folder / folder_name))
             # 调试输出
             print(Fore.YELLOW, idx, end="")
             print(Fore.BLUE, folder_name, end="")
             print(Fore.GREEN, "send to", receiver, f"{subject} - 作业反馈")
-            # print(Fore.RESET, content)
+            print(Fore.RESET, main_score)
             print(Fore.CYAN, att)
             print(Fore.RESET, end="")
             # 发送邮件
